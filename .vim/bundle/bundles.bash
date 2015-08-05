4311e9a18d25008520ef976c9c0ec6e96b426def
Mehmet SOYLU <mssoylu@gmail.com>
2015-08-05 19:26:01 +0300

    Create bundles.bash

---
+++ .vim/bundle/bundles.bash
@@ -0,0 +1,4 @@
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/ervandew/supertab
+git clone https://github.com/Lokaltog/vim-easymotion
