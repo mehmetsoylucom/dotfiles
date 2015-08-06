d7b6ccbdb41cc3986d98703595a5a66bf91912c6
Mehmet SOYLU <mssoylu@gmail.com>
2015-08-06 05:13:14 +0300

    Update bundles.bash

--- .vim/bundle/bundles.bash
+++ .vim/bundle/bundles.bash
@@ -2,3 +2,4 @@ git clone https://github.com/kien/ctrlp.vim
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/ervandew/supertab
 git clone https://github.com/Lokaltog/vim-easymotion
+git clone https://github.com/thoughtbot/vim-rspec
