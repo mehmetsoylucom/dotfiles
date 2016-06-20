a6907fd7567dc30cd2776222999ab249fa01df80
mehmetsoylucom <mehmet@soylu.net>
2016-06-20 22:26:01 +0300

    install.sh added for 14.04

---
+++ ubuntu-14.04/install.sh
@@ -0,0 +1,8 @@
+#
+# Dotfiles
+# Author : Mehmet Soylu
+# OS : Ubuntu 14.04
+# 
+exec sudo cp ../Inconsolata-dz.otf /usr/share/fonts/truetype/Inconsolata-dz.otf
+exec sudo cp .tmux.com ~
+exec sudo cp .bashrc ~
