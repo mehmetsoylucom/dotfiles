c350f1294f3ea02cc34390425c3cb48cba93f398
mehmetsoylucom <mehmet@soylu.net>
2016-06-20 22:44:37 +0300

    Install.sh updated.

--- ubuntu-14.04/install.sh
+++ ubuntu-14.04/install.sh
@@ -3,18 +3,23 @@
 # Author : Mehmet Soylu
 # OS : Ubuntu 14.04
 # 
-exec sudo cp ../Inconsolata-dz.otf /usr/share/fonts/truetype/Inconsolata-dz.otf
-exec sudo cp .tmux.com ~
-exec sudo cp .bashrc ~
+
+# add font
+
+exec sudo cp -rf ../Inconsolata-dz.otf /usr/share/fonts/truetype/Inconsolata-dz.otf
+
+# add config dotfiles for tmux and git
+
+exec sudo cp -rf .tmux.com .bashrc .gitignore .gitconfig ~
 
 # VIM plugins
 
-exec sudo cp -R .vimrc ~
-exec sudo cp -R .vim ~
+exec sudo cp -rf .vimrc ~
+exec sudo cp -Rrf .vim ~
 exec sudo cd ~/.vim/bundle
-exec sudo git clone https://github.com/kien/ctrlp.vim
-exec sudo git clone https://github.com/scrooloose/nerdtree
-exec sudo git clone https://github.com/ervandew/supertab
-exec sudo git clone https://github.com/Lokaltog/vim-easymotion
-exec sudo git clone https://github.com/thoughtbot/vim-rspec
+exec sudo rm -R ~/.vim/bundle/ctrlp.vim && sudo git clone https://github.com/kien/ctrlp.vim
+exec sudo rm -R ~/.vim/bundle/nerdtree && sudo git clone https://github.com/scrooloose/nerdtree
+exec sudo rm -R ~/.vim/bundle/supertab && sudo git clone https://github.com/ervandew/supertab
+exec sudo rm -R ~/.vim/bundle/vim-easymotion && sudo git clone https://github.com/Lokaltog/vim-easymotion
+exec sudo rm -R ~/.vim/bundle/vim-rspec && sudo git clone https://github.com/thoughtbot/vim-rspec
 
