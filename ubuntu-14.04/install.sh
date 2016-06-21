1ff8aa039d290576cb9829874e1482fd8b6fe814
mehmetsoylucom <mehmet@soylu.net>
2016-06-21 13:40:55 +0300

    install.sh updated for source rc files.

--- ubuntu-14.04/install.sh
+++ ubuntu-14.04/install.sh
@@ -36,6 +36,5 @@ echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
 source ~/.zshrc
-
-
-
+source ~/.bashrc
+source ~/.vimrc
