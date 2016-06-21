f39dd7e44fa3016ff8cb603c744259c49371560a
mehmetsoylucom <mehmet@soylu.net>
2016-06-21 14:53:58 +0300

    console-data added install.sh

--- ubuntu-14.04/install.sh
+++ ubuntu-14.04/install.sh
@@ -26,7 +26,7 @@ cp -Rrf .vim ~
 # Rbenv
 
 sudo apt-get update 
-sudo apt-get install zsh git tig tmux rake ruby 
+sudo apt-get install zsh git tig tmux rake ruby console-data 
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 cd
