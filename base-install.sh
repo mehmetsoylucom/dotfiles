4c0ab57b0b69b0f59183c4c207fe8cab7504a664
Mehmet SOYLU <mehmet@soylu.net>
2016-09-02 12:30:08 +0300

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -1,18 +1,19 @@
 # Base install for ubuntu 14.04
-
 apt-get update
 
 echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' > /etc/apt/sources.list.d/passenger.list
 echo 'deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main' > /etc/apt/sources.list.d/ruby.list
 echo 'deb-src http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main' > /etc/apt/sources.list.d/ruby-src.list
 
-
 apt-get update
 
-sudo apt-get install -y apache2
+sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+
+sudo apt-get install -y apache2 apache2-threaded-dev libapr1-dev libaprutil1-dev
+sudo apt-get install -y git tig ssh 
 sudo apt-get install -y ruby2.3 ruby2.3-dev 
 sudo apt-get install -y python3 python-pip 
-sudo apt-get -y install build-essential patch
+sudo apt-get install -y mysql-server redis-server sqlite3
 
 # nokogiri packs for rails
 sudo apt-get -y ruby-dev zlib1g-dev liblzma-dev
